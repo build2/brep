2d89c9d6aa0f2fcdd6403c4973f7e7005a6796a0
Boris Kolpackov <boris@codesynthesis.com>
2015-04-09 17:01:45 +0200

    Reimplement apache::service without using virtual functions

--- services.cxx
+++ services.cxx
@@ -11,7 +11,7 @@ using namespace brep;
 using web::apache::service;
 
 static const search search_mod;
-service<search> AP_MODULE_DECLARE_DATA search_srv ("search", search_mod);
+service AP_MODULE_DECLARE_DATA search_srv ("search", search_mod);
 
 static const view view_mod;
-service<view> AP_MODULE_DECLARE_DATA view_srv ("view", view_mod);
+service AP_MODULE_DECLARE_DATA view_srv ("view", view_mod);
