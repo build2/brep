539f22f75167dff773b0afdded39f60a93a2332c
Boris Kolpackov <boris@codesynthesis.com>
2025-04-07 10:22:43 +0200

    Sketch

--- mod/mod-ci-github-service-data.cxx
+++ mod/mod-ci-github-service-data.cxx
@@ -146,6 +146,7 @@ namespace brep
                 string cs,
                 string rs)
       : kind (k), pre_check (pc), re_request (rr),
+        report_mode (report_mode::undetermined), report_budget (10) // @@ TMP
         warning_success (ws),
         installation_access (move (iat_tok), iat_ea),
         app_id (aid),
